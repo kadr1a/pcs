233f2c9ce504b340948b3a4acbdeb9ccca6ac9f9
katusha <katya.musarieva@icloud.com>
2025-09-15 19:40:21 +0300

    Initial commit

--- calculator/obj/Debug/net9.0/calculator.AssemblyInfo.cs
+++ calculator/obj/Debug/net9.0/calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85881618f92b895a50b04e490aec72ddfb369f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ec229e087f023dedd02b528cc2bab52908a86b")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
